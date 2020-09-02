ac42564685e6591746e3d28aa5bc486835e982a8
Mr.Milul <miladmizani@yahoo.com>
2020-09-02 20:55:47 +0300

    not found page done

---
+++ Templates/modules/notfound.php
@@ -0,0 +1,20 @@
+<?php
+
+function get_title(){
+    return "Not Found";
+}
+
+function get_content (){
+    ?>
+        <div class="container">
+            <div class="jumbotron">
+                <h1 class="display-4 text-danger">Not Found Error 404</h1>
+                <h4 class="lead">Sorry There is no Such page</h4>
+                <hr class="my-4">
+            <a class="btn btn-primary btn-lg" href="home" role="button">Home</a>
+        </div>
+    
+</div><?php } 
+
+
+
