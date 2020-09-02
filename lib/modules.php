18b45ea9f1a42f923b2efdabebec29283d6ad964
Mr.Milul <miladmizani@yahoo.com>
2020-09-02 20:59:54 +0300

    some changes applied

--- lib/modules.php
+++ lib/modules.php
@@ -1,13 +1,28 @@
 <?php
 
 function render_page(){
+     
     include_once ('Templates/header.php');
-      $req = requested_url();
-    include_once  ("Templates/modules/".$req.".php");
    include_once ('Templates/nav.php');
+  
     get_content();
     
-   
     include_once ('Templates/body.php');
     
 }
+
+function load_page(){
+    $modole = requested_url(); 
+    if(empty($modole)){
+        return '/home';
+    }
+    if(file_exists("Templates/modules/".$modole.".php")){
+        include_once  ("Templates/modules/".$modole.".php");
+    }else{
+        include_once  ("Templates/modules/notfound.php");
+    }
+    
+    render_page();
+    
+}
+
