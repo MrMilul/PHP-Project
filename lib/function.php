4e1d4ca641b656cb94cffbe2cd124eef18d13f06
Mr.Milul <miladmizani@yahoo.com>
2020-09-03 22:00:21 +0300

    add a function for showing warnigns

--- lib/function.php
+++ lib/function.php
@@ -17,12 +17,20 @@ function requested_url(){
    return $request_ulr;
 }
 
-function message($message, $color = 'danger'){ ?>
 
-    <div class="alert alert-<?php $color ?> alert-dismissible fade show" role="alert">
-        <strong><?php$message?></strong>
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-        </button>
+function redirect_to($url){
+    header("Location:$url");
+    die();
+}
+
+function message($message, $color='danger'){?>
+    <div class="alert alert-<?php echo $color?> alert-dismissible fade show" role="alert">
+        <strong><?php echo "$message"?></strong>
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
     </div>
 <?php }
+
+
+ 
