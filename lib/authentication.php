7161732c0e08c73f8e61b25595677e2bbdf78913
Mr.Milul <miladmizani@yahoo.com>
2020-09-03 20:53:53 +0300

    authentication is ready

---
+++ lib/authentication.php
@@ -0,0 +1,52 @@
+<?php
+
+define('SESSION_EXPIRE_TIME', 24*3600);
+
+session_start();
+
+$current_user = null; 
+$current_user_id = null; 
+
+function get_current_user(){
+    global $current_user; 
+    return $current_user;
+}
+function get_current_user_id(){
+    global $current_user_id; 
+    return $current_user_id;
+}
+
+function is_user_loged_in(){
+    global $current_user_id; 
+    if(isset($current_user_id)){
+        return true;
+    }
+    return false;
+}
+
+function clear_user_session(){
+    unset($_SESSION['useername']);
+    unset($_SESSION['password']);
+    unset($_SESSION['last_access']);
+    unset($_SESSION['id']);
+}
+
+function user_logout(){
+    global $current_user;
+    global $current_user_id; 
+    clear_user_session();
+    $current_user = null;
+    $current_user_id = null;
+}
+
+function user_login($username, $password){
+    $userdata = get_user($username);
+    if($username !== $userdata['username'] && sha1($password) !== $userdata['password']){
+        return false;
+    }
+    global $current_user;
+    global $current_user_id;
+    
+    $current_user = $userdata['username'];
+    $current_user_id = $userdata['id'];
+}
